51e8d2bdb1671a02a78b2cebd33a1e08f48b4a5c
ashok <ashokkumar.a@innoppl.com>
2026-04-23 20:29:51 +0530

    tset

--- assets/js/app-env.js
+++ assets/js/app-env.js
@@ -10,5 +10,5 @@
      */
     window.APP_ENV_LIVE = true;
     window.APP_LOCAL_ALGO_API_BASE_URL = window.APP_LOCAL_ALGO_API_BASE_URL || 'http://localhost:8000/algo';
-    window.APP_LIVE_ALGO_API_BASE_URL = window.APP_LIVE_ALGO_API_BASE_URL || 'http://finedgealgo.com:8000/algo';
+    window.APP_LIVE_ALGO_API_BASE_URL = window.APP_LIVE_ALGO_API_BASE_URL || 'https://finedgealgo.com/algo';
 })();
